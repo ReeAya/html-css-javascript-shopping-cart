d3d16124d3f279bafc9dfe2351cbe4f1bebb2f13
Reezan <100752303+ReeAya@users.noreply.github.com>
2023-06-14 15:09:25 +0900

    Update store.js

--- store.js
+++ store.js
@@ -5,7 +5,6 @@ import { addToCart } from "./shoppingCart.js";
 
 const storeItemTemplate = document.querySelector("#store-item-template");
 const storeItemContainer = document.querySelector("[data-store-container]");
-// const IMAGE_URL = "https://dummyimage.com/420x260";
 
 export function setupStore() {
   if (storeItemContainer == null) return;
@@ -32,7 +31,6 @@ function renderStoreItem(item) {
 
   const image = storeItem.querySelector("[data-image]");
   image.src = item.image;
-  // image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
 
   const price = storeItem.querySelector("[data-price]");
   price.innerText = formatCurrency(item.priceCents / 100);
